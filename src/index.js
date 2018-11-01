75476e0a5b0d4f4ec776a6897b701e6b5ed19248
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-11-01 15:33:33 +0300

    modificatoins
    modifying the react filing system

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { SkiDayCount } from './components/SkiDayCount'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.React = React
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+	<SkiDayCount total={50}
+				 powder={20}
+				 backcountry={10}
+				 goal={100}/>,
+	document.getElementById('react-container')
+)
